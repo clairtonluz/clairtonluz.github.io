897619dab7ca5632a44c72f37229f9d695e73837
clairtonluz <clairton.c.l@gmail.com>
2014-07-11 17:17:30 -0300

    exemploDeDropDown

--- js/bytecom.js
+++ js/bytecom.js
@@ -3,10 +3,56 @@ function setFocus(id) {
 };
 
 $(document).ready(function () {
+	// FUNÇÃO QUE FAZ FUNCIONAR O BOTÃO DE OCULTAR E EXIBIR O MENU
 	$('.show-sidebar').on('click', function (e) {
 		e.preventDefault();
 		$('div#main').toggleClass('sidebar-show');
 		setTimeout(MessagesMenuWidth, 250);
+	});
+	
+	// FUNÇÃO QUE FAZ FUNCIONAR O DROPDOWN DO MENU
+	$('.main-menu').on('click', 'a', function (e) {
+		var parents = $(this).parents('li');
+		var li = $(this).closest('li.dropdown');
+		var another_items = $('.main-menu li').not(parents);
+		another_items.find('a').removeClass('active');
+		another_items.find('a').removeClass('active-parent');
+		if ($(this).hasClass('dropdown-toggle') || $(this).closest('li').find('ul').length == 0) {
+			$(this).addClass('active-parent');
+			var current = $(this).next();
+			if (current.is(':visible')) {
+				li.find("ul.dropdown-menu").slideUp('fast');
+				li.find("ul.dropdown-menu a").removeClass('active')
+			}
+			else {
+				another_items.find("ul.dropdown-menu").slideUp('fast');
+				current.slideDown('fast');
+			}
+		}
+		else {
+			if (li.find('a.dropdown-toggle').hasClass('active-parent')) {
+				var pre = $(this).closest('ul.dropdown-menu');
+				pre.find("li.dropdown").not($(this).closest('li')).find('ul.dropdown-menu').slideUp('fast');
+			}
+		}
+		if ($(this).hasClass('active') == false) {
+			$(this).parents("ul.dropdown-menu").find('a').removeClass('active');
+			$(this).addClass('active')
+		}
+		if ($(this).hasClass('ajax-link')) {
+			e.preventDefault();
+			if ($(this).hasClass('add-full')) {
+				$('#content').addClass('full-content');
+			}
+			else {
+				$('#content').removeClass('full-content');
+			}
+			var url = $(this).attr('href');
+			window.location.hash = url;
+		}
+		if ($(this).attr('href') == '#') {
+			e.preventDefault();
+		}
 	});
 });
 
